8c4a637ab9d98cd3f60ca2ec59a938cb26e4695c
unknown <nguyenthang240803@gmail.com>
2024-04-17 15:07:01 +0700

    Add Resizable

--- app/(dashboard)/dashboard/_components/recent-sale.tsx
+++ app/(dashboard)/dashboard/_components/recent-sale.tsx
@@ -1,14 +1,26 @@
 "use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { SellingProducts } from "./selling-products";
 import { Customers } from "./customers";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 export const RecentSales = () => {
   return (
-    <div className="grid gap-4 grid-cols-2">
-      <SellingProducts />
-      <Customers />
-    </div>
+    <ResizablePanelGroup
+      direction="horizontal"
+      className="flex space-x-3 py-4"
+    >
+      <ResizablePanel defaultSize={60}>
+        <SellingProducts />
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel defaultSize={40}>
+        <Customers />
+      </ResizablePanel>
+    </ResizablePanelGroup>
   );
 };
